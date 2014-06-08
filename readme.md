3ca386437841f8418ae6d4ac3a273aedf78b943a
Dave Man <dmn001@gmail.com>
2014-06-09 00:19:43 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+#Python review extractor for iTunes
+
---
+++ test_requests.py
@@ -0,0 +1,28 @@
+import requests, json
+
+appid = '624555293'
+
+user_agent_string = 'iTunes/11.1.5 (Windows; Microsoft Windows 7 x64 Ultimate Edition Service Pack 1 (Build 7601)) AppleWebKit/537.60.15'
+
+start_index = 0
+end_index = start_index + 1000
+
+url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?id=%s&displayable-kind=11&startIndex=%s&endIndex=%s&sort=4&appVersion=all' % (appid,start_index,end_index)
+
+s = requests.Session()
+
+s.headers.update({'User-Agent':user_agent_string})
+
+print s.headers
+
+r = s.get(url)
+
+j = r.json()
+for item in j['userReviewList']:
+	print item['body']
+
+with open(appid+'.json', 'w') as outfile:
+  json.dump(r.json(), outfile)
+
+
+# body, rating, name, title, voteSum, voteCount, date, userReviewId
