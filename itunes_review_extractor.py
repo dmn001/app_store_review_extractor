3e387b7d0aab5966868964ecd4b21e81534553b0
Dave Man <dmn001@gmail.com>
2014-06-09 00:37:22 +0100

    get review count

--- itunes_review_extractor.py
+++ itunes_review_extractor.py
@@ -5,21 +5,32 @@ class itunes_review_extractor():
         self.appid = appid
         self.user_agent_string = 'iTunes/11.1.5 (Windows; Microsoft Windows 7 x64 Ultimate Edition Service Pack 1 (Build 7601)) AppleWebKit/537.60.15'
         self.start_index = 0
-        self.end_index = self.start_index + 1000
-        self.url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?id=%s&displayable-kind=11&startIndex=%s&endIndex=%s&sort=4&appVersion=all' % (self.appid,self.start_index,self.end_index)
+        self.end_index = self.start_index + 100
+        
         self.session = requests.Session()
         self.session.headers.update({'User-Agent':self.user_agent_string})
 
-    def get_url(self,url=None):
-        if url is None:
-            url = self.url
-        self.response = self.session.get(url)
+        self.review_count = 0
 
+    def get_review_info(self):
+        self.url = 'https://itunes.apple.com/us/customer-reviews/id%s?dataOnly=true&displayable-kind=11&appVersion=all' % (self.appid)
+        self.response = self.session.get(self.url)
         self.json_string = self.response.json()
+        return self.json_string
 
-    def print_reveiews(self):
-        for item in self.json_string['userReviewList']:
-            print item['body']
+    def get_review_count(self):
+        self.get_review_info()
+        print "totalNumberOfReviews =", self.json_string['totalNumberOfReviews']
+
+    def get_reviews(self):
+        self.url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?id=%s&displayable-kind=11&startIndex=%s&endIndex=%s&sort=4&appVersion=all' % (self.appid,self.start_index,self.end_index)
+        self.response = self.session.get(self.url)
+        self.json_string = self.response.json()
+        return self.json_string
+
+    # def print_reviews(self):
+    #     for item in self.json_string['userReviewList']:
+    #         print item['body']
 
     def output_to_file(self):
         with open(self.appid+'.json', 'w') as outfile:
--- test_itunes_review_extractor.py
+++ test_itunes_review_extractor.py
@@ -2,6 +2,11 @@ from itunes_review_extractor import itunes_review_extractor
 
 e = itunes_review_extractor('624555293')
 
-e.get_url()
-# e.print_reveiews()
-e.output_to_file()
+# e.get_reviews()
+# e.print_reviews()
+# e.output_to_file()
+
+# json = e.get_review_info()
+# print json
+
+e.get_review_count()
