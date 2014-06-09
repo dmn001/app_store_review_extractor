4fa756da71ace5ebdc65c4113a6220cc1e8b2589
Dave Man <dmn001@gmail.com>
2014-06-09 01:21:20 +0100

    append to list

--- itunes_review_extractor.py
+++ itunes_review_extractor.py
@@ -1,4 +1,5 @@
-import requests, json
+# -*- coding: utf-8 -*-
+import requests, json, csv
 
 class itunes_review_extractor():
     def __init__(self,appid):
@@ -12,6 +13,9 @@ class itunes_review_extractor():
         self.session.headers.update({'User-Agent':self.user_agent_string})
 
         self.review_count = 0
+        self.results = []
+
+        self.csv_fh = None
 
     def get_review_info(self):
         self.url = 'https://itunes.apple.com/us/customer-reviews/id%s?dataOnly=true&displayable-kind=11&appVersion=all' % (self.appid)
@@ -31,17 +35,41 @@ class itunes_review_extractor():
         else:
             self.update_end_index()
         self.url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?id=%s&displayable-kind=11&startIndex=%s&endIndex=%s&sort=4&appVersion=all' % (self.appid,start_index,self.end_index)
+        print self.url
         self.response = self.session.get(self.url)
         self.json_string = self.response.json()
         return self.json_string
 
     def get_all_reviews(self):
-        self.review_count = 1234
+        self.review_count = 350
+        self.output_csv_init()
         while(self.start_index < self.review_count):
             self.update_end_index()
             print self.start_index, self.end_index
+            json_string = self.get_reviews()
             self.start_index = self.start_index + self.range
+            self.append_json_to_results(json_string)
+            # self.output_append_to_csv(json_string)
+        self.output_json_to_file()
 
+    def append_json_to_results(self,json_string):
+        self.results = self.results + json_string['userReviewList']
+        for r in json_string['userReviewList']:
+            pass
+            # print r['body'],r['rating'],r['name'],r['title'],r['voteSum'],r['voteCount'],r['date'],r['userReviewId']
+
+    def output_csv_init(self):
+        self.csv_fh = open(self.appid+'.csv', 'wb')
+        csv_header = ['body', 'rating', 'name', 'title', 'voteSum', 'voteCount', 'date', 'userReviewId']
+        wr = csv.writer(self.csv_fh, quoting=csv.QUOTE_ALL)
+        wr.writerow(csv_header)
+
+    def output_append_to_csv(self,json_string):
+        for r in json_string['userReviewList']:
+            out_list = [r['body'],r['rating'],r['name'],r['title'],r['voteSum'],r['voteCount'],r['date'],r['userReviewId']]
+            out_list=[str(s).encode('utf-8') for s in out_list]
+            wr = csv.writer(self.csv_fh, quoting=csv.QUOTE_ALL)
+            wr.writerow(out_list)
 
     def update_end_index(self):
         self.end_index = self.start_index + self.range - 1
@@ -50,9 +78,13 @@ class itunes_review_extractor():
     #     for item in self.json_string['userReviewList']:
     #         print item['body']
 
-    def output_to_file(self):
+    def output_json_to_file(self):
+        # print self.results
+        # for item in self.results:
+            # print item
         with open(self.appid+'.json', 'w') as outfile:
-          json.dump(self.response.json(), outfile)
+          json.dump(self.results, outfile)
+          # json.dump(self.response.json(), outfile)
 
 
         # body, rating, name, title, voteSum, voteCount, date, userReviewId
